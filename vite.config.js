2d89cf7575e70956159e8879d4a534f62eb3cb1c
istone-you <istoneyou1205@yahoo.co.jp>
2026-03-28 00:35:54 +0900

    coverageのチェックスクリプトを追加

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+    },
     environment: "happy-dom",
     globals: true,
   },
